cd1f6a3761248a3f08e0eeeeeb8d9fc4124e021d
Aaron Nech <aaronnech@gmail.com>
2014-12-12 15:01:35 -0800

    fixed

--- server.ts
+++ server.ts
@@ -1,17 +1,12 @@
-///<reference path='def/node.d.ts' />
-///<reference path='def/express.d.ts' />
+///<reference path="./def/node.d.ts"/>
+///<reference path="./def/express.d.ts"/>
 
-import http = require("http")
-import path = require("path")
-import express = require("express")
+import express = require('express');
 
 var app = express();
 
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.static(path.join(__dirname, 'client')));
+app.get('/', function(req, res) {
+    res.send('hi');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-});
+app.listen(3000);
