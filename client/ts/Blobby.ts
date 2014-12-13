de73e6d65cac6a018c981231af1bba1ff688094f
Aaron Nech <aaronnech@gmail.com>
2014-12-12 16:18:48 -0800

    fixed

--- client/ts/Blobby.ts
+++ client/ts/Blobby.ts
@@ -1,11 +1,16 @@
 ///<reference path="../../def/phaser.d.ts"/>
 
+import ServerSocket = require('./ServerSocket');
+
 class Blobby {
 
+    private static SERVER_ADDRESS : string = 'localhost:3000';
     private game: Phaser.Game;
+    private server : ServerSocket;
 
 
     constructor() {
+        this.server = new ServerSocket(Blobby.SERVER_ADDRESS);
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
 
--- client/ts/ServerSocket.ts
+++ client/ts/ServerSocket.ts
@@ -25,3 +25,5 @@ class ServerSocket {
     }
 
 }
+
+export = ServerSocket;
