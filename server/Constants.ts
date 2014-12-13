5b5d98e4f6ac64ce5639ec61d31b28cd342a68a9
Daniel Gorrie <dgorrie@Daniels-MacBook-Pro.local>
2014-12-12 17:16:48 -0800

    mon changes

--- server/Constants.ts
+++ server/Constants.ts
@@ -4,6 +4,11 @@ class Constants {
 	public static WORLD_HEIGHT : number = 600;
 	public static DEFAULT_BLOB_MASS : number = 100.0;
 	public static MAX_INITIAL_VELOCITY : number = 15;
+	public static BLOB_TYPE : any = {
+		USER : 0,
+		DUMMY : 1,
+		JETTISONED : 2
+	}
 }
 
 export = Constants;
