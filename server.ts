9da709fe27e5d62de90b0d91dca3cdb30623b19d
Aaron Nech <aaronnech@gmail.com>
2014-12-12 16:10:48 -0800

    fixed

--- server.ts
+++ server.ts
@@ -2,9 +2,16 @@
 ///<reference path="./def/express.d.ts"/>
 
 import express = require('express');
+import sockets = require('socket.io');
+import http = require('http');
 
+import Blobby = require('./server/Blobby');
+
+// Create app and server static
 var app = express();
-
 app.use(express.static(__dirname + '/client'));
 
-app.listen(3000);
+var server = new http.createServer(app).listen(3000);
+
+// create game
+var game = new Blobby(sockets.listen(server));
